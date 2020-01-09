b6aad9ab41cdb67ea4ac262b36f6867c4563e90a
Anna Gerdii <annagerdiy@gmail.com>
2020-01-09 18:06:07 +0200

    Updates

---
+++ precache-manifest.8747d6dd3ea6be65d022ce8539599b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff1438555caf7885ab49f879b5eac1c",
+    "url": "/react_redux-list-of-posts/index.html"
+  },
+  {
+    "revision": "9fef552e66ab960fc96c",
+    "url": "/react_redux-list-of-posts/static/css/main.e92b7b01.chunk.css"
+  },
+  {
+    "revision": "d550a18d2c4a524cb03a",
+    "url": "/react_redux-list-of-posts/static/js/2.61c35c33.chunk.js"
+  },
+  {
+    "revision": "9fef552e66ab960fc96c",
+    "url": "/react_redux-list-of-posts/static/js/main.38670827.chunk.js"
+  },
+  {
+    "revision": "c3092fe5bcf6cd34b5b5",
+    "url": "/react_redux-list-of-posts/static/js/runtime~main.d0e8944a.js"
+  }
+]);
